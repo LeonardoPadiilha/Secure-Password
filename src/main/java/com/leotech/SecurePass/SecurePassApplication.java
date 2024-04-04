7571ddebfc152fc587339d435e082c8b81926090
Leonardo Padilha <leoprsouza@hotmail.com>
2024-04-04 17:50:14 -0300

    first commit

---
+++ src/main/java/com/leotech/SecurePass/SecurePassApplication.java
@@ -0,0 +1,13 @@
+package com.leotech.SecurePass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurePassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurePassApplication.class, args);
+	}
+
+}
